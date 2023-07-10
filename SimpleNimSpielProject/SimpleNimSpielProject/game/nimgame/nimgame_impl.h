e0f9a706dd59d00a5706281a911c412eb5651b8e
Jo Wagner <jowagner@limago.de>
2023-07-10 15:19:18 +0200

    einzeiliger Block

--- SimpleNimSpielProject/SimpleNimSpielProject/game/nimgame/nimgame_impl.h
+++ SimpleNimSpielProject/SimpleNimSpielProject/game/nimgame/nimgame_impl.h
@@ -18,48 +18,59 @@ namespace atlas::game {
         }
 
     private:
-         auto play_round()-> void {
+         auto play_round()-> void {// Integration
             human_move();
             computer_move();
         }
 
         auto human_move()-> void {
 
-            int move;
+            if(is_gameover()) return;
+            execute_move();
+            terminate_move( "Mensch");
+        }
+
+        auto execute_move()->void  {
             while(true) {
                 std::cout << "Es gibt " << stones << " Steine. Bitte nehmen Sie 1,2 oder 3." << std::endl;
                 std::cin >> move;
-                if( move >= 1 && move <= 3) break;
+                if(move >= 1 && move <= 3) break;
                 std::cout << "Ungueltiger Zug!" << std::endl;
             }
-            stones -= move;
-
         }
 
-        auto  computer_move()-> void {
-            const int moves[] = {3,1,1,2};
-            int move;
 
-            if(stones < 1) {
-                 std::cout << "Du Loser" << std::endl;
-                 return;
-            }
-            if(stones == 1) {
-                std::cout << "Du hast nur Glueck gehabt!" << std::endl;
-                return;
-            }
+        auto  computer_move()-> void {
+
+            if(is_gameover()) return;
+
+            const int moves[] = {3,1,1,2};
 
             move = moves[stones % 4];
             std::cout << "Computer nimmt" << move << " Steine." << std::endl;
 
-            stones -= move;
+            terminate_move("Computer");
         }
 
-        auto is_gameover() -> bool {
+        void terminate_move( std::string player) {
+            update_board();
+            print_gameover_message_if_game_is_over(player);
+        }
+
+        void print_gameover_message_if_game_is_over(const std::string &player) {
+            if(is_gameover()) {
+                std::cout << player << " hat verloren" << std::endl;
+            }
+        }
+
+        // -------- Sumpf -------------------
+        auto update_board()-> void { stones -= move; }
+
+        auto is_gameover() -> bool { // Operation
             return stones < 1;
         };
         int stones;
-
+        int move;
 
     };
 
